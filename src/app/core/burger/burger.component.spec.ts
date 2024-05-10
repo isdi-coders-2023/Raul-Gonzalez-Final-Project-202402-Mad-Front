e28bc45eab24c784736f20efe9925cce468e46b2
raulgonzalez <raulgonji@gmail.com>
2024-05-10 17:39:42 +0200

    Update testing

--- src/app/core/burger/burger.component.spec.ts
+++ src/app/core/burger/burger.component.spec.ts
@@ -1,78 +1,23 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// import BurgerComponent from './burger.component';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-
-// describe('BurgerComponent', () => {
-//   let component: BurgerComponent;
-//   let fixture: ComponentFixture<BurgerComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [BurgerComponent, HttpClientTestingModule],
-//     }).compileComponents();
-
-//     fixture = TestBed.createComponent(BurgerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import BurgerComponent from './burger.component';
-import { StateService } from '../../service/state.service';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BurgerComponent', () => {
   let component: BurgerComponent;
   let fixture: ComponentFixture<BurgerComponent>;
-  let stateServiceSpy: jasmine.SpyObj<StateService>;
-  let routerSpy: jasmine.SpyObj<Router>;
-  let locationSpy: jasmine.SpyObj<Location>;
 
   beforeEach(async () => {
-    // Creamos spies para las dependencias del componente
-    const stateServiceSpyObj = jasmine.createSpyObj('StateService', ['']);
-    const routerSpyObj = jasmine.createSpyObj('Router', ['navigate', 'url']);
-    const locationSpyObj = jasmine.createSpyObj('Location', ['back']);
-
     await TestBed.configureTestingModule({
-      declarations: [BurgerComponent],
-      providers: [
-        { provide: StateService, useValue: stateServiceSpyObj },
-        { provide: Router, useValue: routerSpyObj },
-        { provide: Location, useValue: locationSpyObj },
-      ],
+      imports: [BurgerComponent, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BurgerComponent);
     component = fixture.componentInstance;
-
-    // Obtenemos los spies de las dependencias
-    stateServiceSpy = TestBed.inject(
-      StateService
-    ) as jasmine.SpyObj<StateService>;
-    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
-    locationSpy = TestBed.inject(Location) as jasmine.SpyObj<Location>;
+    fixture.detectChanges();
   });
 
-  it('should create the component', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should navigate to "/menu" when not on "/menu"', () => {
-    routerSpy.url = '/some-other-route';
-    component.onClick();
-    expect(routerSpy.navigate).toHaveBeenCalledWith(['/menu']);
-  });
-
-  it('should go back when on "/menu"', () => {
-    routerSpy.url = '/menu';
-    component.onClick();
-    expect(locationSpy.back).toHaveBeenCalled();
   });
 });
